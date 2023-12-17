464d1d579612ed15a1da5556eaa7c0866c03d99a
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-12-17 22:40:31 +0000

    relationship

--- src/models.py
+++ src/models.py
@@ -36,17 +36,21 @@ class User(Base):
     name = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    followers = relationship('Follower', back_populates='user')
 
 class Follower(Base):
-    __tablename__ = 'follower'
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))    
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='followers')    
 
 class Post(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     user_id = Column(ForeignKey('user.id'))
+    media = relationship('Media', back_populates='posts')
+    comments = relationship('Comment', back_populates='posts')      
  
 class Media(Base):
     __tablename__ = 'media'
@@ -55,14 +59,17 @@ class Media(Base):
     type = Column(String(250), nullable=False)    
     url = Column(String(250), nullable=False) 
     post_id = Column(String(250), ForeignKey('post.id')) 
+    posts = relationship('Post', back_populates='media')   
+
 
 class Comment(Base):
-    __tablename__ = 'comment'
+    __tablename__ = 'comments'
     
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)            
     author_id = Column(String(250), nullable=False)  
-    post_id = Column(String(250), ForeignKey('post.id'))  
+    post_id = Column(String(250), ForeignKey('post.id'))
+    posts = relationship('Post', back_populates='comments')   
 
     def to_dict(self):
         return {}
